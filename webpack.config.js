e82d786f52f95ac300b32912b7b7d74eccaf2080
Alan Kydd <akydd@ualberta.net>
2016-08-26 02:31:34 +0000

    Added more setup files.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var config = {
+  entry: 'index.jsx',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
