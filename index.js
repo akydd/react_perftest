e10b2126614debe5d7f6b65e27e2294e9b6021c9
Alan Kydd <alan@betable.com>
2016-09-12 13:39:00 -0600

    Added perf tool.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
+import Perf from 'react-addons-perf'
 import App from './app.js'
 import configureStore from './configureStore.js'
 
 const store = configureStore()
 
-render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('test')
-)
+function run() {
+    window.Perf = Perf
+
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById('test')
+    )
+}
+
+window.addEventListener('DOMContentLoaded', run)
